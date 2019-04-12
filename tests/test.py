2ca674aae29710ccf7da1fe36b81cca2d361ce74
tell-k <ffk2005@gmail.com>
2019-04-13 00:10:49 +0900

    Use pytest as new test runner.

--- tests/test.py
+++ tests/test.py
@@ -5,7 +5,6 @@ from __future__ import (division, print_function,
 import mock
 import os
 import sys
-import unittest
 import yaml
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -33,34 +32,33 @@ TARGETS = [
 ]
 
 
-class WootheeTest(unittest.TestCase):
+class TestWoothee:
 
     def test_contains_constants(self):
         from woothee import dataset
-        self.assertEqual(dataset.ATTRIBUTE_NAME, 'name')
+        assert dataset.ATTRIBUTE_NAME == 'name'
 
     def test_contains_const_list(self):
         from woothee import dataset
-        self.assertEqual(
-            dataset.ATTRIBUTE_LIST,
-            [dataset.ATTRIBUTE_NAME, dataset.ATTRIBUTE_CATEGORY,
-             dataset.ATTRIBUTE_OS, dataset.ATTRIBUTE_VENDOR,
-             dataset.ATTRIBUTE_VERSION, dataset.ATTRIBUTE_OS_VERSION]
-        )
-        self.assertEqual(
-            dataset.CATEGORY_LIST,
-            [dataset.CATEGORY_PC, dataset.CATEGORY_SMARTPHONE,
-             dataset.CATEGORY_MOBILEPHONE, dataset.CATEGORY_CRAWLER,
-             dataset.CATEGORY_APPLIANCE, dataset.CATEGORY_MISC,
-             dataset.VALUE_UNKNOWN]
-        )
+
+        assert dataset.ATTRIBUTE_LIST == ([dataset.ATTRIBUTE_NAME,
+                                           dataset.ATTRIBUTE_CATEGORY,
+                                           dataset.ATTRIBUTE_OS,
+                                           dataset.ATTRIBUTE_VENDOR,
+                                           dataset.ATTRIBUTE_VERSION,
+                                           dataset.ATTRIBUTE_OS_VERSION])
+
+        assert dataset.CATEGORY_LIST == [dataset.CATEGORY_PC,
+                                         dataset.CATEGORY_SMARTPHONE,
+                                         dataset.CATEGORY_MOBILEPHONE,
+                                         dataset.CATEGORY_CRAWLER,
+                                         dataset.CATEGORY_APPLIANCE,
+                                         dataset.CATEGORY_MISC,
+                                         dataset.VALUE_UNKNOWN]
 
     def test_contains_dataset(self):
         from woothee import dataset
-        self.assertEqual(dataset.get('GoogleBot')['name'], 'Googlebot')
-
-    def test_should_be_read_from_each_modules_correctly(self):
-        pass
+        assert dataset.get('GoogleBot')['name'] == 'Googlebot'
 
     def test_testsets(self):
         import woothee
@@ -87,11 +85,7 @@ class WootheeTest(unittest.TestCase):
                                 attribute, e['target']
                             ))
                             if _can_assert(attribute, e):
-                                self.assertEqual(
-                                    r[attribute],
-                                    e[attribute],
-                                    testname
-                                )
+                                assert r[attribute] == e[attribute], testname
 
     def test_non_provide_testsets(self):
         # This test pattern that does not exist in testsets.
@@ -101,151 +95,151 @@ class WootheeTest(unittest.TestCase):
         import woothee
 
         # 48 line in lib/woothee/appliance.py
-        self.assertEqual({
+        assert {
             "name": "Nintendo DSi",
             "version": "UNKNOWN",
             "os": "Nintendo DSi",
             "os_version": "UNKNOWN",
             "category": "appliance",
             "vendor": "Nintendo",
-        }, woothee.parse("(Nintendo DSi; U; ja)"))
+        } == woothee.parse("(Nintendo DSi; U; ja)")
 
         # 50 line in lib/woothee/appliance.py
-        self.assertEqual({
+        assert {
             "name": "Nintendo Wii",
             "version": "UNKNOWN",
             "os": "Nintendo Wii",
             "os_version": "UNKNOWN",
             "category": "appliance",
             "vendor": "Nintendo",
-        }, woothee.parse("(Nintendo Wii; U; ; 3642; ja)"))
+        } == woothee.parse("(Nintendo Wii; U; ; 3642; ja)")
 
         # 26 line lib/woothee/browser.py
-        self.assertEqual({
+        assert {
             "name": "Internet Explorer",
             "version": "11.0",
             "os": "Windows Phone OS",
             "os_version": "8.1",
             "category": "smartphone",
             "vendor": "Microsoft",
-        }, woothee.parse(
+        } == woothee.parse(
             "Mozilla/5.0 (Windows Phone 8.1; ARM; Trident/7.0;"
             " Touch; IEMobile/11.0; NOKIA; Lumia 930) like Gecko"
-        ))
+        )
 
         # 159 line lib/woothee/crawler.py
-        self.assertEqual({
+        assert {
             "name": "UNKNOWN",
             "version": "UNKNOWN",
             "os": "UNKNOWN",
             "os_version": "UNKNOWN",
             "category": "UNKNOWN",
             "vendor": "UNKNOWN",
-        }, woothee.parse("Data-Hotel-Cat/1.1"))
+        } == woothee.parse("Data-Hotel-Cat/1.1")
 
         # 74-75 line lib/woothee/mobilephone.py
-        self.assertEqual({
+        assert {
             "name": "SymbianOS",
             "version": "UNKNOWN",
             "os": "SymbianOS",
             "os_version": "UNKNOWN",
             "category": "mobilephone",
             "vendor": "UNKNOWN",
-        }, woothee.parse("SymbianOS/9.2;"))
+        } == woothee.parse("SymbianOS/9.2;")
 
         # 78-80 line lib/woothee/mobilephone.py
-        self.assertEqual({
+        assert {
             "name": "Mobile Transcoder",
             "version": "Hatena",
             "os": "Mobile Transcoder",
             "os_version": "UNKNOWN",
             "category": "mobilephone",
             "vendor": "UNKNOWN",
-        }, woothee.parse(
+        } == woothee.parse(
             "(compatible; Hatena-Mobile-Gateway/1.2;"
             " +http://mgw.hatena.ne.jp/help)"
-        ))
+        )
 
         # 25-27 line lib/woothee/os.py
-        self.assertEqual({
+        assert {
             "name": "UNKNOWN",
             "version": "UNKNOWN",
             "os": "Windows UNKNOWN Ver",
             "os_version": "UNKNOWN",
             "category": "pc",
             "vendor": "UNKNOWN",
-        }, woothee.parse(
+        } == woothee.parse(
             "Mozilla/5.0 (Windows ; rv:8.0) Gecko/20111105 Thunderbird/8.0"
-        ))
+        )
 
         # 49 line lib/woothee/os.py
-        self.assertEqual({
+        assert {
             "name": "Internet Explorer",
             "version": "UNKNOWN",
             "os": "Windows NT 4.0",
             "os_version": "NT 4.0",
             "category": "pc",
             "vendor": "Microsoft",
-        }, woothee.parse(
+        } == woothee.parse(
             "Mozilla/4.0 (compatible; MSIE 6.0b; Windows NT 4.0)"
-        ))
+        )
 
         # 51 line lib/woothee/os.py
-        self.assertEqual({
+        assert {
             "name": "Internet Explorer",
             "version": "6.0",
             "os": "Windows 98",
             "os_version": "98",
             "category": "pc",
             "vendor": "Microsoft",
-        }, woothee.parse(
+        } == woothee.parse(
             "Mozilla/4.0 (compatible; MSIE 6.0; Windows 98)"
-        ))
+        )
 
         # 53 line lib/woothee/os.py
-        self.assertEqual({
+        assert {
             "name": "Internet Explorer",
             "version": "5.50",
             "os": "Windows 95",
             "os_version": "95",
             "category": "pc",
             "vendor": "Microsoft",
-        }, woothee.parse(
+        } == woothee.parse(
             "Mozilla/4.0 (compatible; MSIE 5.50; Windows 95; SiteKiosk 4.8)"
-        ))
+        )
 
         # 121 line lib/woothee/os.py
-        self.assertEqual({
+        assert {
             "name": "UNKNOWN",
             "version": "UNKNOWN",
             "os": "iPad",
             "os_version": "UNKNOWN",
             "category": "smartphone",
             "vendor": "UNKNOWN",
-        }, woothee.parse("Mozilla/5.0 (iPad; "))
+        } == woothee.parse("Mozilla/5.0 (iPad; ")
 
         # 123 line lib/woothee/os.py
-        self.assertEqual({
+        assert {
             "name": "UNKNOWN",
             "version": "UNKNOWN",
             "os": "iPod",
             "os_version": "UNKNOWN",
             "category": "smartphone",
             "vendor": "UNKNOWN",
-        }, woothee.parse("Mozilla/5.0 (iPod; "))
+        } == woothee.parse("Mozilla/5.0 (iPod; ")
 
         # 183-185 line lib/woothee/os.py
-        self.assertEqual({
+        assert {
             "name": "Mobile Transcoder",
             "version": "Naver",
             "os": "Mobile Transcoder",
             "os_version": "UNKNOWN",
             "category": "mobilephone",
             "vendor": "UNKNOWN",
-        }, woothee.parse("Naver Transcoder"))
+        } == woothee.parse("Naver Transcoder")
 
 
-class TestIsCrawler(unittest.TestCase):
+class TestIsCrawler:
 
     def _getTarget(self):
         from woothee import is_crawler
@@ -255,22 +249,22 @@ class TestIsCrawler(unittest.TestCase):
         return self._getTarget()(*args, **kwargs)
 
     def test_false(self):
-        self.assertFalse(self._callFUT(""))
-        self.assertFalse(self._callFUT("-"))
-        self.assertFalse(self._callFUT(None))
-        self.assertFalse(self._callFUT(
+        assert not self._callFUT("")
+        assert not self._callFUT("-")
+        assert not self._callFUT(None)
+        assert not self._callFUT(
             "Mozilla/5.0 (Windows NT 6.3; "
             "Trident/7.0; rv:11.0) like Gecko"
-        ))
+        )
 
     def test_true(self):
-        self.assertTrue(self._callFUT(
+        assert self._callFUT(
             "Mozilla/5.0 (compatible; Yahoo! Slurp;"
             " http://help.yahoo.com/help/us/ysearch/slurp)"
-        ))
+        )
 
 
-class TestTryRareCases(unittest.TestCase):
+class TestTryRareCases:
     """ challenge_smartphone_patterns in try_rare_cases is never return True.
     Because, "CFNetwork" is caught by the challenge_smartphone in try_os.
     Therefore, I have prepared the individual a test case
@@ -288,10 +282,6 @@ class TestTryRareCases(unittest.TestCase):
     @mock.patch("woothee.browser.challenge_sleipnir")
     def test_challenge_smartphone_patterns(self, mock_sleipnir):
         result = {}
-        self.assertTrue(self._callFUT("CFNetwork/", result))
-        self.assertEqual({'category': 'smartphone', 'os': 'iOS'}, result)
-        self.assertFalse(mock_sleipnir.called)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert self._callFUT("CFNetwork/", result)
+        assert {'category': 'smartphone', 'os': 'iOS'} == result
+        assert not mock_sleipnir.called
