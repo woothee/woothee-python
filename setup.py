3fc5ae80e44646b2cb2c5532e57e9d7e745b83b7
Hideo Hattori <hattori.hideo@panair.jp>
2020-02-13 15:45:50 +0900

    use zipp 1.1.0

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
     platforms='any',
     install_requires=install_requires,
     setup_requires=['PyYAML>=3.10', 'pytest-runner'],
-    tests_require=['pytest', 'pytest-cov', 'pytest-mock'],
+    tests_require=['pytest', 'pytest-cov', 'pytest-mock', 'zipp==1.1.0'],
     long_description=long_description,
     classifiers=classifiers,
     keywords=['web', 'user-agent', 'parser'],
